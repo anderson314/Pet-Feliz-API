e35a8c0b78ff689acc035ccc1d3da55608f6d555
Anderson Rodrigo <andersonrodrigo03.14@gmail.com>
2021-03-04 17:26:58 -0300

    Restauração

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,19 +10,6 @@ namespace PetFelizApi.Data
         {
 
         }   
-
-        public DbSet<EnderecoDogWalker> EnderecoDogwalker { get; set; }
-        public DbSet<DogWalker> DogWalker { get; set; }
-        public DbSet<Proprietario> Proprietario { get; set; }
-        public DbSet<EnderecoProprietario> EnderProprietario { get; set; }
-        public DbSet<SolicitacaoServico> SolicitacaoServico { get; set; }
-        public DbSet<Cao> Cao      { get; set; }
-        public DbSet<CaoSolicitacao> CaoSolicitacoes        { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<CaoSolicitacao>()
-                .HasKey(cs => new { cs.CaoId, cs.SolicitacaoServicoId});
-        }
         
     }
 }
